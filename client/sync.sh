8a4697c8517d693c517781851d36402a1f96ef15
neilellis <hello@neilellis.me>
2015-07-18 00:13:45 +0100

    Initial Work

--- client/sync.sh
+++ client/sync.sh
@@ -15,7 +15,7 @@ server_port=${SERVER_PORT_873_TCP_PORT}
 
 mkdir -p /.synctmp
 
-rsync --port=$server_port -r --quiet --delete-after --temp-dir=/.synctmp  --timeout=60 --backup --backup-dir=${local_dir}/.syncbackup  --exclude ".sync*"   /shared/  root@${server}::share
+rsync --port=${server_port} -r --quiet --delete-after --temp-dir=/.synctmp  --backup --backup-dir=${local_dir}/.syncbackrev  --exclude ".sync*" root@${server}::share /shared/
 inotifywait -e modify -e create -e move -e delete -e attrib -e close_write -t 3600 -r /shared/
 touch ${local_dir}/.synclast
-rsync --port=${server_port} -r --quiet --delete-after --temp-dir=/.synctmp  --backup --backup-dir=${local_dir}/.syncbackrev  --exclude ".sync*" root@${server}::share /shared/
+rsync --port=$server_port -r --quiet --delete-after --temp-dir=/.synctmp  --timeout=60 --backup --backup-dir=${local_dir}/.syncbackup  --exclude ".sync*"   /shared/  root@${server}::share
